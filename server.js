6f606785ce500103ff905a6e9a5f9cd47d13b8c0
venegasjosh <47758513+venegasjosh@users.noreply.github.com>
2020-02-16 17:56:07 -0800

    switched database to mongodb atlas

--- server.js
+++ server.js
@@ -10,20 +10,31 @@ var server = app.listen(8000, function() {
 })
 
 const fileRoutes = require('./server/config/file-upload')
+const mongoConfig = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true, 
+  useCreateIndex: true
+}
+mongoose.connect("mongodb+srv://josh:adrienc00l@cluster0-7awvp.mongodb.net/test",{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongo DB Connected..."))
+    .catch(err => console.log(err));
 
 app.use('/api/v1/',fileRoutes);
 
 app.use(bodyParser.json());
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+
 
 app.use(express.static( __dirname + "/public/dist/public" ));
 
-require("./server/config/mongoose.js");
+// require("./server/config/mongoose.js");
 require("./server/config/routes.js")(app);
 
 app.all("*", (req,res,next) => {
     res.sendFile(path.resolve("./public/dist/public/index.html"))
 });
+
+
 
 
 
